58a5f621911064cead5b40904af1ff1ff5410021
unmanbearpig <unmanbearpig@gmail.com>
2022-07-14 15:05:59 +0400

    Fix transforming Rails message to MailerSend

--- lib/mailer.rb
+++ lib/mailer.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
-
 require 'railtie'
 
 module MailersendRails
   class Mailer
     def initialize(config)
-      puts "MailersendRails config: #{config}"
+      # No config yet
     end
 
     # Sends the email and returns result
@@ -30,23 +29,32 @@ module MailersendRails
     def msg_to_ms_msg(msg)
       ms_msg = Mailersend::Email.new
       ms_msg.add_subject(msg.subject)
-      ms_msg.recipients = msg.to
+      ms_msg.recipients = msg.to.map { |email| {email: email, name: email}}
       ms_msg.add_from(msg.from[0])
       ms_msg.add_html(msg_html(msg))
       ms_msg.add_text(msg_text(msg))
       ms_msg.ccs = msg.cc
       ms_msg.bcc = msg.bcc
+
+      if ms_msg.text.nil? && ms_msg.html.nil?
+        Rails.logger.warn("Trying to send a message \
+without plaintext and html: #{msg}")
+      end
       ms_msg
     end
 
     # Get plaintext from Rails email msg
     def msg_text(msg)
       if msg.multipart?
-        return msg.text.part
+        text = msg.parts
+          .find { |p| p.content_type =~ /^text\/plain[;$]/ }
+          &.body
+          &.raw_source
+        return text
       end
 
       if msg.mime_type =~ /^text\/plain$/i
-        return msg
+        return msg.body.raw_source
       end
 
       nil
@@ -57,11 +65,15 @@ module MailersendRails
     # Get html from Rails email msg
     def msg_html(msg)
       if msg.multipart?
-        return msg.html.part
+        html = msg.parts
+          .find { |p| p.content_type =~ /^text\/html[;$]/ }
+          &.body
+          &.raw_source
+        return html
       end
 
       if msg.mime_type =~ /^text\/html$/i
-        return msg
+        return msg.body.raw_source
       end
 
       nil
